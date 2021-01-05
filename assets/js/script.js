532048f48b982634e117c0b6252b9ada1db753ac
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-01-04 17:57:56 -0800

    basic functionality achieved.  form unable to submit without input data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,45 @@
 var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
 
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!")
+        return false;
+    };
+
+    formE1.reset();
+
+    // package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    // send it as an argument to createTaskE1
+    createTaskE1(taskDataObj);
+                                                       
+};
+
+var createTaskE1 = function(taskDataObj) {
+    // create list item
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task.";
+    // create div to hold task info and list item
+    var taskInfoE1 = document.createElement("div");
+    // give the div a class name
+    taskInfoE1.className = "task-info";
+    // add HTML content to div
+    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    listItemE1.appendChild(taskInfoE1);
+    
+    // add entire list item to list
     tasksToDoE1.appendChild(listItemE1);
 };
 
-formE1.addEventListener("submit", createTaskHandler);
+formE1.addEventListener("submit", taskFormHandler);
